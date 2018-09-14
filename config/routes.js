055a3bcf8b153b65ecebfbb599dc13f8b014bf9f
Charles Cortinas <crcortinas@gmail.com>
2018-09-14 13:21:54 -0500

    Register
    Implemented registrations

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,53 @@
-const axios = require('axios');
+const axios = require("axios");
+const knex = require("knex");
+const bcrypt = require("bcryptjs");
+const dbConfig = require("../knexfile");
+const jwt = require("jsonwebtoken");
 
-const { authenticate } = require('./middlewares');
+const jwtKey = require("../_secrets/keys").jwtKey;
+const { authenticate } = require("./middlewares");
+const db = knex(dbConfig.development);
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.get("/api/users", getUsers);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
+function getUsers(req, res) {
+  db("users")
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
+
 function register(req, res) {
-  // implement user registration
+  let user = req.body;
+  user.password = bcrypt.hashSync(user.password, 12);
+
+  db("users")
+    .insert(user)
+    .then(ids => {
+      id = ids[0];
+
+      db("users")
+        .where({ id: id })
+        .first()
+        .then(user => {
+          const token = generateToken(user);
+          res.status(201).json({ id: user.id, token });
+        })
+        .catch(err => {
+          res.status(500).send(err);
+        });
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
 }
 
 function login(req, res) {
@@ -19,12 +57,26 @@ function login(req, res) {
 function getJokes(req, res) {
   axios
     .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
+      "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten"
     )
     .then(response => {
       res.status(200).json(response.data);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
+}
+
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+    department: user.department
+  };
+
+  const options = {
+    expiresIn: "1hr",
+    jwtid: "12345"
+  };
+
+  return jwt.sign(payload, jwtKey, options);
 }
